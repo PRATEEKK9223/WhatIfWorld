178abd2547f336b1605af524a9622b0b213aa97a
PRATEEKK9223 <pratheekkpratheekk@gmail.com>
2025-11-08 07:20:05 +0530

    post delete feature is added

--- routes/communityRoutes.js
+++ routes/communityRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router= express.Router();
 import Community from "../Models/community.js";
 import asyncWrap from "../utils/asyncWrap.js";
+import {isLoggedIn} from "../utils/middlewares.js";
 
 
 
@@ -29,6 +30,22 @@ router.get("/community",asyncWrap(async(req,res)=>{
     const posts = await Community.find({}).sort({ sharedAt: -1 }).populate('result').populate('author');
     //  console.log(posts);
     res.render("./Components/community",{title:"community-WhatIfWorld",posts});   
+}));
+
+
+router.get("/delete/:id",isLoggedIn,asyncWrap(async(req,res)=>{
+    let {id}=req.params;
+    let post =await Community.findById(id);
+    if(!post){
+        return res.status(404).json("post not Found");
+    }
+    if(post.author.equals(req.user._id)){
+        await Community.findByIdAndDelete(id);
+        console.log("deleted succesfully");
+        res.redirect("/community");
+    }else{
+        res.send("you not author of this post");
+    }
 }));
 
 
